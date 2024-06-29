0f60b6faca7a1b871aac0a6a252ff47ad8c24173
jglad <jakub.gladysz1@gmail.com>
2024-06-29 19:44:59 +0200

    login, register, send note

---
+++ src/lib/Modal.svelte
@@ -0,0 +1,72 @@
+<script>
+  export let showModal; // boolean
+
+  let dialog; // HTMLDialogElement
+
+  $: if (dialog && showModal) dialog.showModal();
+
+  export function closeModal() {
+    dialog.close();
+  }
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events a11y-no-noninteractive-element-interactions -->
+<dialog
+  bind:this={dialog}
+  on:close={() => (showModal = false)}
+  on:click|self={() => dialog.close()}
+>
+  <!-- svelte-ignore a11y-no-static-element-interactions -->
+  <div on:click|stopPropagation>
+    <slot name="header" />
+    <slot />
+    <div class="absolute top-0 right-0 h-16 w-16">
+      <p class="mt-4 text-lg leading-8 text-gray-600 items-end">
+        <button autofocus on:click={() => dialog.close()} >close</button>
+      </p>
+    </div>
+  </div>
+</dialog>
+
+<style lang="postcss">
+    ::content {
+      background-color: rgb(17, 16, 15);
+    }
+  dialog {
+    max-width: 32em;
+    border-radius: 1em;
+    border: none;
+    padding: 0;
+  }
+  dialog::backdrop {
+    background: rgba(0, 0, 0, 0.3);
+  }
+  dialog > div {
+    padding: 1em;
+  }
+  dialog[open] {
+    animation: zoom 0.3s cubic-bezier(0.34, 1.56, 0.64, 1);
+  }
+  @keyframes zoom {
+    from {
+      transform: scale(0.95);
+    }
+    to {
+      transform: scale(1);
+    }
+  }
+  dialog[open]::backdrop {
+    animation: fade 0.2s ease-out;
+  }
+  @keyframes fade {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+  button {
+    display: block;
+  }
+</style>
---
+++ src/lib/authUtils.js
@@ -0,0 +1,54 @@
+import { NDKEvent, NDKPrivateKeySigner } from "@nostr-dev-kit/ndk";
+import { generatePrivateKey } from "nostr-tools";
+import CryptoJS from 'crypto-js';
+
+// todo replace from props
+const PASSPHRASE = '';
+
+export const createNewSigner = () => {
+    const privateKey = generatePrivateKey();
+    const signer = new NDKPrivateKeySigner(privateKey);
+    return signer;
+};
+
+export const recreateSigner = (privateKey) => {
+    const signer = new NDKPrivateKeySigner(privateKey);
+    return signer;
+};
+
+// TODO: this is PoC, must replace with some secure way 
+export const encryptKey = (key) => {
+    return CryptoJS.AES.encrypt(key, PASSPHRASE).toString();
+};
+
+export const decryptKey = (encryptedKey) => {
+    const bytes = CryptoJS.AES.decrypt(encryptedKey, PASSPHRASE);
+    return bytes.toString(CryptoJS.enc.Utf8);
+};
+
+export const register = async () => {
+    const signer = createNewSigner();
+    localStorage.setItem('secure', encryptKey(signer.privateKey));
+    return signer;
+};
+
+export const setName = async (ndk) => {
+    const name = localStorage.getItem('user');
+    const metadataEvent = new NDKEvent(ndk);
+    metadataEvent.kind = 0;
+    const content = JSON.stringify({
+      name: name
+    });
+    metadataEvent.content = content;
+    await metadataEvent.sign();
+    await metadataEvent.publish();
+}
+
+export const getUserProfileName = async (ndk, pubKey) => {
+    const user = ndk.getUser({
+      npub: pubKey,
+    });
+
+    await user.fetchProfile();
+    return user.profile.name || '';
+  }
