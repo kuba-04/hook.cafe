151ad8ac768085de052040e31f8536dfea5f3aa6
jglad <jakub.gladysz1@gmail.com>
2024-08-20 19:49:40 +0200

    trying to fix apple part 2

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,6 @@
 <script lang="ts">
       
-    // TODO with nostr instead:
-    // let session: AuthSession
-  
-    // onMount(() => {
-    //   supabase.auth.getSession().then(({ data }) => { 
-    //     session = data.session
-    //   })
-  
-    //   supabase.auth.onAuthStateChange((_event, _session) => {
-    //     session = _session
-    //   })
-    // })
   </script>
   
   <div class="container" style="padding: 50px 0 100px 0">
-    <!-- {#if !session}
-    <Auth />
-    {:else}
-    <Account {session} />
-    {/if} -->
   </div>
--- src/lib/Env.js
+++
@@ -1,7 +0,0 @@
-// prod
-// export const RELAY_URL = import.meta.env.VITE_RELAY_URL; 
-
-// local
-export const RELAY_URL = "ws://127.0.0.1:8080";
-// export const RELAY_URL = "wss://nostr.mezzely.com";
-// 'wss://relay.damus.io'
