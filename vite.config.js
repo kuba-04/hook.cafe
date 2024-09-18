48812b4dc0fa50b1bb2e4b7e3a9a7d78e459d5be
jglad <jakub.gladysz1@gmail.com>
2024-09-18 19:31:21 +0200

    add polyfill

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import babel from 'vite-plugin-babel';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), babel()	],
+	optimizeDeps: {
+		include: ['bigint-polyfill'], 
+	},	
 });
