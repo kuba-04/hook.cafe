f5286184c36c7c1b44f3bb158f2ff16caf6c623e
jglad <jakub.gladysz1@gmail.com>
2024-09-19 10:42:13 +0200

    Revert "add polyfill"
    This reverts commit 48812b4dc0fa50b1bb2e4b7e3a9a7d78e459d5be.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import babel from 'vite-plugin-babel';
 
 export default defineConfig({
-	plugins: [sveltekit(), babel()	],
-	optimizeDeps: {
-		include: ['bigint-polyfill'], 
-	},	
+	plugins: [sveltekit()]
 });
