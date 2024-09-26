45e96328fbb17dca533f0786dfa782dfa98b44c4
kuba <jakub.gladysz1@gmail.com>
2024-09-26 12:21:56 +0200

    pwa plugin

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import { VitePWA } from 'vite-plugin-pwa';
+import htmlPlugin from "vite-plugin-html-config"
+
 
 export default defineConfig({
 	plugins: [
@@ -8,5 +11,34 @@ export default defineConfig({
 		nodePolyfills({
 			protocolImports: true,
 		  }),
+		  VitePWA({
+			registerType: "autoUpdate",
+			injectRegister: "auto",
+			workbox: {
+			  maximumFileSizeToCacheInBytes: 5 * 1024 ** 2,
+			},
+			manifest: {
+			  name: process.env.VITE_APP_NAME,
+			  short_name: process.env.VITE_APP_NAME,
+			  description: process.env.VITE_APP_DESCRIPTION,
+			  protocol_handlers: [{protocol: "web+nostr", url: "/%s"}],
+			  permissions: ["clipboardRead", "clipboardWrite", "unlimitedStorage"],
+			  icons: [
+				{src: "static/pwa-64x64.png", sizes: "64x64", type: "image/png"},
+				{src: "static/pwa-192x192.png", sizes: "192x192", type: "image/png"},
+				{src: "static/pwa-512x512.png", sizes: "512x512", type: "image/png", purpose: "any"},
+				{
+				  src: "static/maskable-icon-512x512.png",
+				  sizes: "512x512",
+				  type: "image/png",
+				  purpose: "maskable",
+				},
+			  ],
+			},
+		  }),
+		htmlPlugin({
+		// Add necessary meta tags and icons here
+		}),  
+		
 	]
 });
