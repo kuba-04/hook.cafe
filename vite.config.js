8b0e807d2d428d55cc0447daa3a3a90bb4638de0
kuba <jakub.gladysz1@gmail.com>
2024-09-26 12:27:26 +0200

    pwa plugin fixes

--- vite.config.js
+++ vite.config.js
@@ -18,9 +18,10 @@ export default defineConfig({
 			  maximumFileSizeToCacheInBytes: 5 * 1024 ** 2,
 			},
 			manifest: {
-			  name: process.env.VITE_APP_NAME,
-			  short_name: process.env.VITE_APP_NAME,
-			  description: process.env.VITE_APP_DESCRIPTION,
+			  name: "Hook Cafe",
+			  short_name: "HookCafe",
+			  description: "Simple App to connect people for daily meals",
+			  theme_color: "accent:#FC560E,neutral-950:#FAFAFA,neutral-900:#FAFAFA,neutral-800:#F5F5F5,neutral-700:#D4D4D4,neutral-600:#A3A3A3,neutral-500:#737373,neutral-400:#525252,neutral-300:#404040,neutral-200:#262626,neutral-100:#171717,neutral-50:#0A0A0A,success:#12D2B0,tinted-800:#FFFFFF,tinted-700:#FAFAFA,tinted-600:#F5F5F5,tinted-500:#D4D4D4,tinted-400:#A3A3A3,tinted-200:#737373,tinted-100:#525252,warning:#FCAB0E,danger:#dc0c0c",
 			  protocol_handlers: [{protocol: "web+nostr", url: "/%s"}],
 			  permissions: ["clipboardRead", "clipboardWrite", "unlimitedStorage"],
 			  icons: [
